9dac2d4d0dc95516c2d7b65eb682605ad54decf7
emrejz <emre.kayacan@msn.com>
2020-06-20 13:58:33 +0300

    maybe need this wasm import opt

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(
+  express.static(
+    "public"
+    //         , {
+    //     setHeaders: (res, path, stat) => {
+    //       if (path.endsWith(".wasm")) {
+    //         res.setHeader("Content-Type", "application/wasm");
+    //       }
+    //     },
+    //   }
+  )
+);
 
 app.listen(3000, () => console.log("server ok"));
