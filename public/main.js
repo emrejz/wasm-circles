82a2058bf41c1b711e12f97a1232a686adbac66c
emrejz <emre.kayacan@msn.com>
2020-06-21 00:13:48 +0300

    canvas ok

--- public/main.js
+++ public/main.js
@@ -1,3 +1,8 @@
 Module.onRuntimeInitialized = () => {
-  console.log(UTF8ToString(_str()));
+  const canvas = document.querySelector("canvas");
+  const context = canvas.getContext("2d");
+  context.beginPath();
+  context.arc(500, 500, 50, 0, 2 * Math.PI, false);
+  context.fillStyle = "red";
+  context.fill();
 };
