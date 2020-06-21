c6e9105ed1c076765f4ebb8f4c5ae858cad6743a
emrejz <emre.kayacan@msn.com>
2020-06-21 21:10:22 +0300

    a

--- src/server.js
+++ src/server.js
@@ -2,16 +2,13 @@ const express = require("express");
 const app = express();
 
 app.use(
-  express.static(
-    "public"
-    //         , {
-    //     setHeaders: (res, path, stat) => {
-    //       if (path.endsWith(".wasm")) {
-    //         res.setHeader("Content-Type", "application/wasm");
-    //       }
-    //     },
-    //   }
-  )
+  express.static("public", {
+    setHeaders: (res, path, stat) => {
+      if (path.endsWith(".wasm")) {
+        res.setHeader("Content-Type", "application/wasm");
+      }
+    },
+  })
 );
 
 app.listen(3000, () => console.log("server ok"));
