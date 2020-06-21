02a5115131e2ad98f9002e7605aeb853857a6ce5
emrejz <emre.kayacan@msn.com>
2020-06-21 15:01:36 +0300

    created circles by canvas

--- public/main.js
+++ public/main.js
@@ -1,8 +1,27 @@
 Module.onRuntimeInitialized = () => {
   const canvas = document.querySelector("canvas");
   const context = canvas.getContext("2d");
-  context.beginPath();
-  context.arc(500, 500, 50, 0, 2 * Math.PI, false);
-  context.fillStyle = "red";
-  context.fill();
+  const circle = [];
+  const circleGenerator = (i) => {
+    context.beginPath();
+    context.arc(
+      circles[i],
+      circles[i + 1],
+      circles[i + 2],
+      0,
+      2 * Math.PI,
+      false
+    );
+    context.fillStyle = `rgb(${circles[i + 3]},${circles[i + 4]},${
+      circles[i + 5]
+    })`;
+    context.fill();
+  };
+  const render = (num) => {
+    circles = new Int32Array(Module.HEAP32.buffer, _getCircle(num), 6 * num);
+    for (let i = 0; i < circles.length; i += 6) {
+      circleGenerator(i);
+    }
+  };
+  render(10);
 };
