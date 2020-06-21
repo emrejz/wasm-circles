b2ce1ac28189261f0ab18eafe2a315f060819c48
emrejz <emre.kayacan@msn.com>
2020-06-21 17:28:04 +0300

    animate circle func added

--- lib/circle.c
+++ lib/circle.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-
+#define total 500
 struct Circle
 {
     int x;
@@ -10,23 +10,74 @@ struct Circle
     int cg;
     int cb;
 };
-struct Circle *circle;
+struct AnimatedCircle
+{
+    int vx;
+    int vy;
+    int dx;
+    int dy;
+};
+
+struct Circle circles[total];
+struct AnimatedCircle animatedCircles[total];
 int getRand(int limit)
 {
     return rand() % limit;
 };
-struct Circle *getCircle(itr)
+
+void generateCircles()
 {
     srand(time(NULL));
-    for (int i = 0; i < itr; i++)
+    for (int i = 0; i < total; i++)
     {
         int radius = getRand(50);
-        circle[i].x = getRand(1000) + radius;
-        circle[i].y = getRand(600) + radius;
-        circle[i].r = radius;
-        circle[i].cr = getRand(255);
-        circle[i].cg = getRand(255);
-        circle[i].cb = getRand(255);
+        circles[i].x = getRand(700) + radius;
+        circles[i].y = getRand(700) + radius;
+        circles[i].r = radius;
+        circles[i].cr = getRand(255);
+        circles[i].cg = getRand(255);
+        circles[i].cb = getRand(255);
+        animatedCircles[i].vx = getRand(10);
+        animatedCircles[i].vy = getRand(10);
+        animatedCircles[i].dx = getRand(2) == 0 ? -1 : 1;
+        animatedCircles[i].dy = getRand(2) == 0 ? -1 : 1;
     }
-    return circle;
+};
+struct Circle *getCircles(canvasWei, canvasHei)
+{
+
+    for (int i = 0; i < total; i++)
+    {
+        if (animatedCircles[i].dx == 1)
+        {
+            if (circles[i].x + animatedCircles[i].vx + circles[i].r >= canvasWei)
+            {
+                animatedCircles[i].dx = -1;
+            }
+        }
+        if (animatedCircles[i].dx == -1)
+        {
+            if (circles[i].x - animatedCircles[i].vx - circles[i].r <= 0)
+            {
+                animatedCircles[i].dx = 1;
+            }
+        }
+        if (animatedCircles[i].dy == 1)
+        {
+            if (circles[i].y + animatedCircles[i].vy + circles[i].r >= canvasHei)
+            {
+                animatedCircles[i].dy = -1;
+            }
+        }
+        if (animatedCircles[i].dy == -1)
+        {
+            if (circles[i].y - animatedCircles[i].vy - circles[i].r <= 0)
+            {
+                animatedCircles[i].dy = 1;
+            }
+        }
+        circles[i].x = circles[i].x + animatedCircles[i].vx * animatedCircles[i].dx;
+        circles[i].y = circles[i].y + animatedCircles[i].vy * animatedCircles[i].dy;
+    }
+    return circles;
 };
